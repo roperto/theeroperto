8110220b551fe71c0bd6890b732591c18444faf9
Daniel Thee Roperto <daniel@theeroperto.com>
2016-10-19 01:24:01 +1100

    Issue #1 - Basic example of Laraval with PHPUnit and Behat.

---
+++ features/example.feature
@@ -0,0 +1,8 @@
+Feature: Testing
+  In order to test Behat
+  As a programmer
+  I implemented a silly behat feature
+
+  Scenario: Home Page
+    Given I am on the homepage
+    Then I should see "Laravel"
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,15 +4,13 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
-    {
+    public function testBasicExample() {
         $this->visit('/')
              ->see('Laravel');
     }
