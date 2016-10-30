e3371d970c4d784acaf2dc9b4604318a295fa969
Daniel Thee Roperto <daniel@theeroperto.com>
2016-10-30 17:52:36 +1100

    Added 'after_success' script to Travis.
    On successful builds for 'integration' it should set it as the 'production' branch.

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\CacheController;
 
 class CacheTest extends TestCase {
     public function testGetMainMenu() {
-        $this->markTestSkipped('Will be fixed in Issue #19.');
         $path = 'cache/images/daniel/portfolio/LojaGeralNET/categories.100x75.png';
         $publicPath = public_path($path);
         if (file_exists($publicPath)) {
